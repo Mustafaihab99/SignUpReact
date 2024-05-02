c7bc93386f0f80b71cb62fd6149b73c815240234
Mustafa Ihab <mustafaihab9@gmail.com>
2024-05-02 14:08:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Component/Assets/LoginSignup';
+
+function App() {
+  /*const routes = createHashRouters({
+    path: "" ,
+  })*/
+  return (
+    <div>
+    <LoginSignup></LoginSignup>
+    </div>
+  );
+}
+
+export default App;
